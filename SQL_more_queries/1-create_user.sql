7134622071e6b8839e488ae3fe597ad001cc0965
eruhigira-tech <e.ruhigira@alustudent.com>
2026-07-29 01:25:03 -0700

    Task 2

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create the user 'user_0d_1' if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on the server to 'user_0d_1'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the privilege changes
+FLUSH PRIVILEGES;
+
