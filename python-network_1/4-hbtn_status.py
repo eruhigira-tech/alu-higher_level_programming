2bf0603a264b47b41691fe8bb4c07c9f93000b6a
eruhigira-tech <e.ruhigira@alustudent.com>
2026-07-30 00:58:16 -0700

    Updates

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Fetches url
-using the request moduel
-"""
+"""Fetches https://intranet.hbtn.io/status using requests."""
 import requests
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
-    res = requests.get(url)
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
